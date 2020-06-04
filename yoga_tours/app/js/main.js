5e6e88af5ba25ddf7a1cb21aaed45b4706823dcd
Mid12night <atwinternight@gmail.com>
2020-06-04 13:17:11 +0300

    Added slider.

--- yoga_tours/app/js/main.js
+++ yoga_tours/app/js/main.js
@@ -1,6 +1,15 @@
-$(function(){
+$(function () {
 
-
-
+    $('.center').slick({
+        variableWidth: true,
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        speed: 1000,
+        arrows: false,
+    });
 
 });
