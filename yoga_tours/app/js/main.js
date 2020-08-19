20cd6df336462bcac3833180b38f3bd589874a6c
Mid12night <atwinternight@gmail.com>
2020-08-19 15:59:02 +0300

    finished adaptive

--- yoga_tours/app/js/main.js
+++ yoga_tours/app/js/main.js
@@ -1,15 +1,26 @@
 $(function () {
 
     $('.center').slick({
-        variableWidth: true,
         centerMode: true,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
+        centerPadding: '19%',
+        slidesToShow: 1,
         autoplay: true,
-        autoplaySpeed: 7000,
-        speed: 2000,
-        arrows: false
+        autoplaySpeed: 4000,
+        speed: 159,
+        arrows: false,
+        infinite: true,
+        waitForAnimate: false,
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1,
+                infinite:true,
+                autoplay: true,
+                dots:true,
+            }
+        },]
     });
 
     $('.single-item').slick({
@@ -17,16 +28,22 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
-        autoplay: false,
-        autoplaySpeed: 4000,
-        speed: 2000,
-        infinite: true
+        speed: 1000,
+        infinite: true,
+        waitForAnimate: true
     });
 
     $('.comment-slider__inner').slick({
         dots: true,
         infinite: true,
-        arrows:true
+        arrows: true,
+        adaptiveHeight: true,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                arrows: false
+            }
+        },]
     });
 
 });
